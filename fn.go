3d8c09335a2953093c0224b3a48de935fdfa0e51
Grzegorz Lewczuk <extern.lewczuk_grzegorz@allianz.de>
2024-04-25 13:20:39 +0200

    added logic

--- fn.go
+++ fn.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"context"
+	"github.com/crossplane/function-sdk-go/resource/composed"
+	"slices"
+	"strings"
 
-	"github.com/crossplane/crossplane-runtime/pkg/errors"
-	"github.com/crossplane/crossplane-runtime/pkg/logging"
-	"github.com/crossplane/function-in-memory-env/input/v1beta1"
+	"github.com/crossplane/function-sdk-go/errors"
+	"github.com/crossplane/function-sdk-go/logging"
 	fnv1beta1 "github.com/crossplane/function-sdk-go/proto/v1beta1"
 	"github.com/crossplane/function-sdk-go/request"
+	"github.com/crossplane/function-sdk-go/resource"
 	"github.com/crossplane/function-sdk-go/response"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/util/json"
+
+	xpv1alpha1 "github.com/crossplane/crossplane/apis/apiextensions/v1alpha1"
+	extv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 )
 
 // Function returns whatever response you ask it to.
@@ -18,21 +26,83 @@ type Function struct {
 	log logging.Logger
 }
 
+const (
+	FunctionContextKeyEnvironment = "apiextensions.crossplane.io/environment"
+)
+
 // RunFunction runs the Function.
 func (f *Function) RunFunction(_ context.Context, req *fnv1beta1.RunFunctionRequest) (*fnv1beta1.RunFunctionResponse, error) {
 	f.log.Info("Running function", "tag", req.GetMeta().GetTag())
 
 	rsp := response.To(req, response.DefaultTTL)
 
-	in := &v1beta1.Input{}
-	if err := request.GetInput(req, in); err != nil {
-		response.Fatal(rsp, errors.Wrapf(err, "cannot get Function input from %T", req))
+	// Get the desired composite resource from the request.
+	observedComposite, err := request.GetObservedCompositeResource(req)
+	if err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot get observed composite resource from %T", req))
 		return rsp, nil
 	}
 
-	// TODO: Add your Function logic here!
-	response.Normalf(rsp, "I was run with input %q!", in.Example)
-	f.log.Info("I was run!", "input", in.Example)
+	//  Get the desired composed resources from the request.
+	desiredComposed, err := request.GetDesiredComposedResources(req)
+	if err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot get desired composed resources from %T", req))
+		return rsp, nil
+	}
+	f.log.Debug("Found desired resources", "count", len(desiredComposed))
+
+	inMemoryEnvRaw, ok := request.GetContextKey(req, FunctionContextKeyEnvironment)
+	if ok {
+		inputEnv := &unstructured.Unstructured{}
+		if err := resource.AsObject(inMemoryEnvRaw.GetStructValue(), inputEnv); err != nil {
+			response.Fatal(rsp, errors.Wrapf(err, "cannot get Composition environment from %T context key %q", req, FunctionContextKeyEnvironment))
+			return rsp, nil
+		}
+		f.log.Debug("Loaded Composition environment from Function context", "context-key", FunctionContextKeyEnvironment)
+	}
+
+	envConfig := &xpv1alpha1.EnvironmentConfig{}
+	envConfig.Data = make(map[string]extv1.JSON)
+
+	inMemoryEnv := inMemoryEnvRaw.GetStructValue().AsMap()
+
+	keysToSkip := []string{"kind", "apiVersion"}
+	for key, value := range inMemoryEnv {
+		if slices.Contains(keysToSkip, key) {
+			continue
+		}
+
+		jsonBytes, err := json.Marshal(value)
+		if err != nil {
+		}
+
+		envConfig.Data[key] = extv1.JSON{Raw: jsonBytes}
+	}
+
+	envConfigLabels := make(map[string]string)
+	envConfigLabels["xr-apiversion"] = strings.Replace(observedComposite.Resource.GetAPIVersion(), "/", "_", -1)
+	envConfigLabels["xr-kind"] = observedComposite.Resource.GetKind()
+	envConfigLabels["xr-name"] = observedComposite.Resource.GetName()
+
+	f.log.Info("EnvironmentConfig labels", "labels", envConfigLabels)
+	envConfig.SetLabels(envConfigLabels)
+
+	_ = xpv1alpha1.AddToScheme(composed.Scheme)
+	desiredEnvConfig, err := composed.From(envConfig)
+	if err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot convert %T to %T", envConfig, &composed.Unstructured{}))
+		return rsp, nil
+	}
+
+	desiredComposed[resource.Name("in-memory-env")] = &resource.DesiredComposed{Resource: desiredEnvConfig}
+	desiredComposed[resource.Name("in-memory-env")].Ready = resource.ReadyTrue
+
+	f.log.Info("constructed in-memory EnvConfig", "inMemoryEnv:", desiredEnvConfig)
+
+	if err := response.SetDesiredComposedResources(rsp, desiredComposed); err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot set desired composed resources from %T", req))
+		return rsp, nil
+	}
 
 	return rsp, nil
 }
--- go.mod
+++ go.mod
@@ -6,10 +6,12 @@ toolchain go1.21.3
 
 require (
 	github.com/alecthomas/kong v0.9.0
+	github.com/crossplane/crossplane v1.15.2
 	github.com/crossplane/crossplane-runtime v1.15.1
 	github.com/crossplane/function-sdk-go v0.2.0
 	github.com/google/go-cmp v0.6.0
 	google.golang.org/protobuf v1.33.0
+	k8s.io/apiextensions-apiserver v0.29.1
 	k8s.io/apimachinery v0.29.3
 	sigs.k8s.io/controller-tools v0.14.0
 )
@@ -31,7 +33,7 @@ require (
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/gnostic-models v0.6.8 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
-	github.com/google/uuid v1.4.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -45,14 +47,13 @@ require (
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/mod v0.14.0 // indirect
-	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/net v0.23.0 // indirect
 	golang.org/x/oauth2 v0.15.0 // indirect
-	golang.org/x/sys v0.16.0 // indirect
-	golang.org/x/term v0.16.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/term v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 	golang.org/x/tools v0.17.0 // indirect
@@ -63,7 +64,6 @@ require (
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/api v0.29.1 // indirect
-	k8s.io/apiextensions-apiserver v0.29.1 // indirect
 	k8s.io/client-go v0.29.1 // indirect
 	k8s.io/klog/v2 v2.110.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20231010175941-2dd684a91f00 // indirect
