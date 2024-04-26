2188b217a6f51ab24ac3719887c025ac42e4ef12
Grzegorz <lewczuk.grzeg@gmail.com>
2024-04-26 13:43:30 +0200

    Implement function in-memory-env (#1)

--- example/README.md
+++ example/README.md
@@ -10,16 +10,23 @@ $ go run . --insecure --debug
 
 ```shell
 # Then, in another terminal, call it with these example manifests
-$ crossplane beta render xr.yaml composition.yaml functions.yaml -r
+$ crossplane beta render xr.yaml composition.yaml functions.yaml --context-values=apiextensions.crossplane.io/environment='{"key": "value"}' -r
 ---
 apiVersion: example.crossplane.io/v1
 kind: XR
 metadata:
   name: example-xr
 ---
-apiVersion: render.crossplane.io/v1beta1
-kind: Result
-message: I was run with input "Hello world"!
-severity: SEVERITY_NORMAL
-step: run-the-template
+apiVersion: apiextensions.crossplane.io/v1alpha1
+data:
+  key: value
+kind: EnvironmentConfig
+metadata:
+  annotations:
+    crossplane.io/composition-resource-name: in-memory-env
+  labels:
+    crossplane.io/composite: example-xr
+    xr-apiversion: example.crossplane.io_v1
+    xr-kind: XR
+    xr-name: example-xr
 ```
--- fn.go
+++ fn.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"context"
+	"slices"
+	"strings"
 
-	"github.com/crossplane/crossplane-runtime/pkg/errors"
-	"github.com/crossplane/crossplane-runtime/pkg/logging"
-	"github.com/crossplane/function-in-memory-env/input/v1beta1"
+	extv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/util/json"
+
+	xpv1alpha1 "github.com/crossplane/crossplane/apis/apiextensions/v1alpha1"
+	"github.com/crossplane/function-sdk-go/errors"
+	"github.com/crossplane/function-sdk-go/logging"
 	fnv1beta1 "github.com/crossplane/function-sdk-go/proto/v1beta1"
 	"github.com/crossplane/function-sdk-go/request"
+	"github.com/crossplane/function-sdk-go/resource"
+	"github.com/crossplane/function-sdk-go/resource/composed"
 	"github.com/crossplane/function-sdk-go/response"
 )
 
@@ -18,21 +26,90 @@ type Function struct {
 	log logging.Logger
 }
 
+const (
+	functionContextKeyEnvironment = "apiextensions.crossplane.io/environment"
+
+	annotationKeyInMemoryEnvEnabled = "inmemoryenv.fn.crossplane.io/enabled"
+)
+
 // RunFunction runs the Function.
 func (f *Function) RunFunction(_ context.Context, req *fnv1beta1.RunFunctionRequest) (*fnv1beta1.RunFunctionResponse, error) {
 	f.log.Info("Running function", "tag", req.GetMeta().GetTag())
 
 	rsp := response.To(req, response.DefaultTTL)
 
-	in := &v1beta1.Input{}
-	if err := request.GetInput(req, in); err != nil {
-		response.Fatal(rsp, errors.Wrapf(err, "cannot get Function input from %T", req))
+	// Get the desired composite resource from the request.
+	observedComposite, err := request.GetObservedCompositeResource(req)
+	if err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot get observed composite resource from %T", req))
 		return rsp, nil
 	}
 
-	// TODO: Add your Function logic here!
-	response.Normalf(rsp, "I was run with input %q!", in.Example)
-	f.log.Info("I was run!", "input", in.Example)
+	if inMemoryEnvEnabled, found := observedComposite.Resource.GetAnnotations()[annotationKeyInMemoryEnvEnabled]; inMemoryEnvEnabled != "true" || !found {
+		f.log.Debug("In-memory environment config not enabled")
+		return rsp, nil
+	}
+
+	//  Get the desired composed resources from the request.
+	desiredComposed, err := request.GetDesiredComposedResources(req)
+	if err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot get desired composed resources from %T", req))
+		return rsp, nil
+	}
+	f.log.Debug("Found desired resources", "count", len(desiredComposed))
+
+	inMemoryEnvRaw, ok := request.GetContextKey(req, functionContextKeyEnvironment)
+	if ok {
+		inputEnv := &unstructured.Unstructured{}
+		if err := resource.AsObject(inMemoryEnvRaw.GetStructValue(), inputEnv); err != nil {
+			response.Fatal(rsp, errors.Wrapf(err, "cannot get Composition environment from %T context key %q", req, functionContextKeyEnvironment))
+			return rsp, nil
+		}
+		f.log.Debug("Loaded Composition environment from Function context", "context-key", functionContextKeyEnvironment)
+	}
+
+	envConfig := f.createEnvConfig(inMemoryEnvRaw.GetStructValue().AsMap())
+
+	envConfigLabels := make(map[string]string)
+	envConfigLabels["xr-apiversion"] = strings.ReplaceAll(observedComposite.Resource.GetAPIVersion(), "/", "_")
+	envConfigLabels["xr-kind"] = observedComposite.Resource.GetKind()
+	envConfigLabels["xr-name"] = observedComposite.Resource.GetName()
+
+	envConfig.SetLabels(envConfigLabels)
+
+	_ = xpv1alpha1.AddToScheme(composed.Scheme)
+	desiredEnvConfig, err := composed.From(envConfig)
+	if err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot convert %T to %T", envConfig, &composed.Unstructured{}))
+		return rsp, nil
+	}
+
+	desiredComposed[("in-memory-env")] = &resource.DesiredComposed{Resource: desiredEnvConfig}
+	desiredComposed[("in-memory-env")].Ready = resource.ReadyTrue
+
+	f.log.Info("constructed in-memory EnvConfig", "inMemoryEnv:", desiredEnvConfig)
+
+	if err := response.SetDesiredComposedResources(rsp, desiredComposed); err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot set desired composed resources from %T", req))
+		return rsp, nil
+	}
 
 	return rsp, nil
+}
+
+func (f *Function) createEnvConfig(data map[string]interface{}) *xpv1alpha1.EnvironmentConfig {
+	envConfig := &xpv1alpha1.EnvironmentConfig{}
+	envConfig.Data = make(map[string]extv1.JSON)
+
+	keysToSkip := []string{"kind", "apiVersion"}
+	for key, value := range data {
+		if slices.Contains(keysToSkip, key) {
+			continue
+		}
+
+		jsonBytes, _ := json.Marshal(value)
+		envConfig.Data[key] = extv1.JSON{Raw: jsonBytes}
+	}
+
+	return envConfig
 }
